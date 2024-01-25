cd9ef4c4fc91eefa0be5031467e5d665bc490d8b
Itachi9811 <dheerajdeshwal9811@gmail.com>
2024-01-25 13:14:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const moment = require('moment');
-// var mysql = require("mysql");
 
+const hostname = "sql6.freesqldatabase.com";
+const database = "sql6679479";
+const port = "3306";
+const username = "sql6679479";
+const password = "11m5DZDu9u";
 
-
-var hostname = "sql6.freesqldatabase.com";
-var database = "sql6679479";
-var port = "3306";
-var username = "sql6679479";
-var password = "11m5DZDu9u";
 const app = express();
-var con = mysql.createConnection({
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: hostname,
   user: username,
   password,
@@ -20,79 +20,100 @@ var con = mysql.createConnection({
   port,
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+app.get('/lb/cw', (req, res) => {
+  const startOfWeek = moment().startOf('week').format('YYYY-MM-DD HH:mm:ss');
+  const endOfWeek = moment().endOf('week').format('YYYY-MM-DD HH:mm:ss');
+  const query = 'SELECT * FROM leaderboard WHERE TimeStamp BETWEEN ? AND ? ORDER BY Score DESC LIMIT 200';
 
-con.query("SELECT 1+1").on("result", function (row) {
-  console.log(row);
-});
+  pool.getConnection((err, con) => {
+    if (err) {
+      console.error('Error acquiring a connection:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
 
-app.get('/lb/cw', ( req,res) => {
-   const startOfWeek = moment().startOf('week').format('YYYY-MM-DD HH:mm:ss');
-   const endOfWeek = moment().endOf('week').format('YYYY-MM-DD HH:mm:ss');
-   const query = 'SELECT * FROM leaderboard WHERE TimeStamp BETWEEN ? AND ? ORDER BY Score DESC LIMIT 200';
- 
-   con.query(query, [startOfWeek, endOfWeek], (err, results) => {
-     if(err){
-       console.error('Error querying the database:', err);
-       res.status(500).send('Internal Server Error');
-     } 
-     else{
-      res.json(results);
-     }
-   });
- });
+    con.query(query, [startOfWeek, endOfWeek], (err, results) => {
+      con.release(); // Release the connection back to the pool
 
- app.get('/lb/lw/:country', (req, res) => {
-   const country = req.params.country;
-   const startOfLastWeek = moment().subtract(1, 'weeks').startOf('week').format('YYYY-MM-DD HH:mm:ss');
-   const endOfLastWeek = moment().subtract(1, 'weeks').endOf('week').format('YYYY-MM-DD HH:mm:ss');
-   const query = `SELECT * FROM leaderboard WHERE Country = ? AND TimeStamp BETWEEN ? AND ? ORDER BY Score DESC LIMIT 200`;
-   
-   con.query(query, [country, startOfLastWeek, endOfLastWeek], (err, results) => {
-       if (err){
-       console.error('Error querying the database:', err);
-       res.status(500).send('Internal Server Error');
-       } 
-      else if(results.length == 0){
-         console.log("Country not found");
-         res.status(404).send('Country not found');
-      } 
-      else {
-         res.json(results);
+      if (err) {
+        console.error('Error querying the database:', err);
+        res.status(500).send('Internal Server Error');
+        return;
       }
-       
-   });
-   });
 
-   app.get('/ur/:userId', (req, res) => {
-      const userId = req.params.userId;
-      const query = `SELECT (
-        SELECT COUNT(DISTINCT Score) + 1
-        FROM leaderboard AS t2
-        WHERE t2.Score > t1.Score
-      ) AS player_rank
-      FROM leaderboard AS t1
-      WHERE UID = ?
-      ORDER BY Score DESC`;
-  
-      con.query(query, [userId], (err, results) => {
-        if(err){
-          console.error('Error querying the database:', err);
-          res.status(500).send('Internal Server Error');
-        } 
-        else if(results.length == 0){
-          res.status(404).send('User ID not found');
-        } 
-        else{
-         res.json(results);
-        }
-      });
+      res.json(results);
     });
+  });
+});
 
-  
-app.listen(4000,()=>{
-    console.log("Server is runnig on Port: 4000")
-})
+app.get('/lb/lw/:country', (req, res) => {
+  const country = req.params.country;
+  const startOfLastWeek = moment().subtract(1, 'weeks').startOf('week').format('YYYY-MM-DD HH:mm:ss');
+  const endOfLastWeek = moment().subtract(1, 'weeks').endOf('week').format('YYYY-MM-DD HH:mm:ss');
+  const query = `SELECT * FROM leaderboard WHERE Country = ? AND TimeStamp BETWEEN ? AND ? ORDER BY Score DESC LIMIT 200`;
+
+  pool.getConnection((err, con) => {
+    if (err) {
+      console.error('Error acquiring a connection:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    con.query(query, [country, startOfLastWeek, endOfLastWeek], (err, results) => {
+      con.release(); // Release the connection back to the pool
+
+      if (err) {
+        console.error('Error querying the database:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      if (results.length === 0) {
+        console.log("Country not found");
+        res.status(404).send('Country not found');
+      } else {
+        res.json(results);
+      }
+    });
+  });
+});
+
+app.get('/ur/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const query = `SELECT (
+      SELECT COUNT(DISTINCT Score) + 1
+      FROM leaderboard AS t2
+      WHERE t2.Score > t1.Score
+    ) AS player_rank
+    FROM leaderboard AS t1
+    WHERE UID = ?
+    ORDER BY Score DESC`;
+
+  pool.getConnection((err, con) => {
+    if (err) {
+      console.error('Error acquiring a connection:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    con.query(query, [userId], (err, results) => {
+      con.release(); // Release the connection back to the pool
+
+      if (err) {
+        console.error('Error querying the database:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      if (results.length === 0) {
+        res.status(404).send('User ID not found');
+      } else {
+        res.json(results);
+      }
+    });
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on Port: 4000");
+});
